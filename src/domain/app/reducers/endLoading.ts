4f15889b18e175aca38223a9f0b2fb409f786920
VasanthSM <vasanthsubburaj99@gmail.com>
2025-08-09 10:49:02 +0530

    create a folder and move to that folder

--- src/domain/app/reducers/endLoading.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-param-reassign -- immer */
-import type { State } from './State';
-
-const endLoading = (
-  state: State,
-) => {
-  state.loading -= 1;
-};
-export default endLoading;
--- src/domain/app/reducers/startBlocking.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-param-reassign -- immer */
-import type { State } from './State';
-
-const startBlocking = (
-  state: State,
-) => {
-  state.blocking += 1;
-};
-export default startBlocking;
