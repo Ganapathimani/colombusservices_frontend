45a53add4f4abea6525fad8923ede177a7230c9c
VasanthSM <vasanthsubburaj99@gmail.com>
2025-08-09 11:21:18 +0530

    Revert "react-initial-setup"
    This reverts commit 40b44e33e9ab3976d72fdcce0d0f1f25c81b1002.

--- src/domain/app/reducers/State.ts
+++
@@ -1,5 +0,0 @@
-export type State = {
-  loading: number,
-  blocking: number,
-  loadingNames: string[],
-};
--- src/window.d.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-  interface Window {
-    envs: Record<string, string>
-  }
-}
-
-// so that TypeScript picks this up
-export {};
